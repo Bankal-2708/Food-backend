04bd17e5678be587e520d0c7e54ad3d47e28f70e
Bankal-2708 <bankalmavi1@gmail.com>
2026-07-04 00:13:25 +0530

    approve for vercel

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
-    'https://your-actual-frontend-domain.vercel.app'
+     'https://food-delivery-app-bankal2708s-projects.vercel.app'
   ],
   credentials: true
 }));
